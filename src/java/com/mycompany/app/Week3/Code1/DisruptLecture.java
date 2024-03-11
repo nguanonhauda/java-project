579ad870b6f2e2bd56834b064e45f37257a6ca13
nguanonhauda <23010359@st.phenikaa-uni.edu.vn>
2024-03-11 11:22:48 +0700

    push

---
+++ src/java/com/mycompany/app/Week3/Code1/DisruptLecture.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.Week3.Code1;
+
+public class DisruptLecture {
+    class CellPhone{
+        CellPhone(){
+        }
+        public void ring (Tune t ){t.play();}
+    }
+    class Tune {
+        public void play(){
+            System.out.println("Tune.play");
+        }
+    }
+    class ObnoxiusTune extends Tune {
+        public ObnoxiusTune(){}
+    }
+}
